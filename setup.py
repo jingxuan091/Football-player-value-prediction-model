e7e65706487c7e133cfe5113298cf7573defbe44
Andrea Pasti <andreapasti91@gmail.com>
2023-03-19 03:02:28 +0100

    updated package

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
+from setuptools import find_packages
 from setuptools import setup
 
-setup(name='shop',
-      description="we are testing",
-      packages=["shop"])
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='taxifare',
+      version="0.0.10",
+      description="TaxiFare Model (automate_model_lifecycle)",
+      author="Andrea, Andrew, Jin",
+      #url="https://github.com/lewagon/taxi-fare",
+      install_requires=requirements,
+      packages=find_packages()
+      #test_suite="tests",
+      # include_package_data: to install data from MANIFEST.in
+      #include_package_data=True,
+      #zip_safe=False
+      )
